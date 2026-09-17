4493f6ba523894e2603fd4cc809bae4488bac321
chs000 <101612094+chs000-00@users.noreply.github.com>
2026-09-17 09:40:00 -0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.kotlinxSerialization)
+}
+
+group = "me.user"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    macosArm64()
+    linuxArm64()
+    linuxX64()
+    mingwX64()
+
+    targets.withType<KotlinNativeTarget>().configureEach {
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
+    }
+
+    sourceSets {
+        nativeMain.dependencies {
+            implementation(libs.kotlinxSerializationJson)
+        }
+    }
+}
